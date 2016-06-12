20143be9d35d6f1f8492fe6e2df097691d0868c1
ParvatiPatel <paro.patel@rocketmail.com>
2016-06-12 11:30:12 -0400

    Added delete functionality

--- FLesson-4C/Students.aspx.cs
+++ FLesson-4C/Students.aspx.cs
@@ -18,6 +18,8 @@ namespace FLesson_4C
             // if loading the page for the first time, populate the student grid
             if (!IsPostBack)
             {
+                Session["SortColumn"] = "StudentID";
+                Session["SortDirection"] = "A";
                 // Get the student data
                 this.GetStudents();
             }
@@ -44,6 +46,54 @@ namespace FLesson_4C
                 StudentsGridView.DataSource = Students.ToList();
                 StudentsGridView.DataBind();
             }
+        }
+        protected void StudentsGridView_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            // store which row was clicked
+            int selectedRow = e.RowIndex;
+
+            // get the selected StudentID using the Grid's DataKey Collection
+            int StudentID = Convert.ToInt32(StudentsGridView.DataKeys[selectedRow].Values["StudentID"]);
+
+            // use EF to find the selected student from DB and remove it
+            using (DefaultConnection db = new DefaultConnection())
+            {
+                Student deletedStudent = (from studentRecords in db.Students
+                                          where studentRecords.StudentID == StudentID
+                                          select studentRecords).FirstOrDefault();
+
+                // perform the removal in the DB
+                db.Students.Remove(deletedStudent);
+                db.SaveChanges();
+
+                // refresh the grid
+                this.GetStudents();
+
+            }
+        }
+
+        protected void StudentsGridView_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            // Set the new page number
+            StudentsGridView.PageIndex = e.NewPageIndex;
+
+            // refresh the grid
+            this.GetStudents();
+        }
+
+        protected void PageSizeDropDownList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // set new page size
+            StudentsGridView.PageSize = Convert.ToInt32(PageSizeDropDownList.SelectedValue);
+
+            // refresh the grid
+            this.GetStudents();
+        }
+
+       
+        protected void StudentsGridView_Sorting(object sender, GridViewSortEventArgs e)
+        {
+
         }
     }
 }
